b9b5cbea23cc9e9009a8479e95fa05c9b28b2d35
Akshansh Vaishnav <akshanshvaishnavpaypal@gmail.com>
2024-07-06 02:29:39 +0530

    optimised JSON

--- Breezer/src/assets/Body.jsx
+++ Breezer/src/assets/Body.jsx
@@ -2,33 +2,29 @@ import React from "react";
 
 function Body() {
 
-  const [apiLink,setApiLink]=React.useState("");
   const [city,setCity]=React.useState("");
+  const apiKey = "62419947539d4eb083a200928240407";
 
-  function handleLink(){
-    setApiLink("http://api.weatherapi.com/v1/search.json?key=62419947539d4eb083a200928240407&q="+city);
-  }
-
-  function handleCity(event){
+  function handelCity(event){
     setCity(event.target.value);
   }
 
   async function handleResult(){
 
     try {
-     const rawData= await fetch(apiLink);
+     const rawData= await fetch("https://api.weatherapi.com/v1/forecast.json?key="+apiKey+"&q="+city+"&days=5&aqi=yes&alerts=yes");
      const data= await rawData.json();
      console.log(data);
     } catch (error) {
-     console.log("api is not able to fetch data 🫠🫠"); 
+     console.log("api is not able to fetch data 🫠🫠");
+     console.log(error);
     }
   }
 
     return(
         <>
-          <input type="text" name="city" id="searchBar" onChange={handleCity} value={city}/>
-          <button className="searchBtn" onClick={handleLink}>Search</button>
-          <button className="resultBtn" onClick={handleResult}>getResult</button>
+          <input type="text" name="city" id="searchBar" onChange={handelCity} value={city}/>
+          <button onClick={handleResult}>Search</button>
         </>
     );
 }
