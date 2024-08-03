f9cc4f1ba1ca361f8b6c4594591ec1aa5d226ac5
Akshansh Vaishnav <akshanshvaishnavpaypal@gmail.com>
2024-08-04 04:28:04 +0530

    finishing

--- Breezer/src/assets/TodayForecast.jsx
+++ Breezer/src/assets/TodayForecast.jsx
@@ -6,10 +6,10 @@ function TodayForecast({propData}){
         <>
          {propData?.forecast?.forecastday[0]?.hour.map((cast, index) => (
            <div key={index} className='hourlyData'>
-              <p>{cast?.time}</p>
-              <p>{cast?.temp_c}</p>
+              <p className='castTime'>{cast?.time}</p>
+              <p className='castTemp'>{cast?.temp_c}</p>
               <img src={cast?.condition?.icon} alt="weather-icon" />
-              <p>{cast?.condition?.text}</p>
+              <p className='castText'>{cast?.condition?.text}</p>
            </div>
          ))}
         </>
